4cb961b9584ca5d94752652aa48e96976ce9c0fc
Aiden <amb@aspy.dev>
2023-07-30 02:36:14 -0400

    Static Files

---
+++ README.md
@@ -0,0 +1,7 @@
+# ThirdPartyVencordPlugins
+Monorepo for other's plugins. Please open a PR if you would like to dispute or add anything.
+
+# To properly receive the entirety of the repository, please use the following command:
+```
+git clone https://github.com/SomeAspy/ThirdPartyVencordPlugins --recurse-submodules
+```
---
+++ repack.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Repack plugins that are incorrectly organized
+
+### vc-gif-collections
+cp -r git/vc-gif-collections dist
+
+### vc-message-logger-enhanced
+cp -r git/vc-message-logger-enhanced dist
+
+### vc-spotimbed
+cp -r git/vc-spotimbed dist
+
+### vencord-cuteanimeboys
+cp -r git/vencord-cuteanimeboys dist
+
+### betterbencordplugins
+cp git/betterbencordplugins/typso.ts dist
+
+### uwuifier-vencord
+cp git/uwuifier-vencord/uwuifier.ts dist
+
+### vencord-plugins-exhq
+cp git/vencord-plugins-exhq/* dist
+mv dist/WYFSI.ts dist/wyfsi.ts
+
+### vencord-plugins-hypeddomi
+cp git/vencord-plugins-hypeddomi/GlobalBadges/globalBadges.tsx dist/globalbadges.tsx
+cp git/vencord-plugins-hypeddomi/Keyboard-Sounds/keyboardSounds.ts dist/keyboardsounds.ts
+
+### multistickers
+cp -r git/venkordplugins/multistickers dist
